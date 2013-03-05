1e8e8dcf7c5d1d22555bda13701f68fc7cc29860
jessicayuen <jyuen@ualberta.ca>
2013-03-05 16:29:25 -0700

    add recipe again

---
+++ RecipeFinder/src/team13/cmput301/recipefinder/Recipe.java
@@ -0,0 +1,17 @@
+package team13.cmput301.recipefinder;
+
+import java.util.List;
+
+public class Recipe {
+	String name;
+	String description;
+	String author;
+	float rating;
+	List<String> ingredients;
+	List<Photo> photos;
+	
+	public Recipe(String name, String description, String author,
+			List<String> ingredients, List<Photo> photos, float rating) {
+		
+	}
+}
