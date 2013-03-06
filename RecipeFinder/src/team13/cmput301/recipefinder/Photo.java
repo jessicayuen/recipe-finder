2cc869a533fc5145a298d1e4a4574db1a9536e95
hwen3@ualberta.ca <hwen3@ualberta.ca>
2013-03-05 17:09:14 -0700

    updated and finished photo class

--- RecipeFinder/src/team13/cmput301/recipefinder/Photo.java
+++ RecipeFinder/src/team13/cmput301/recipefinder/Photo.java
@@ -1,10 +1,56 @@
 package team13.cmput301.recipefinder;
 
-import android.graphics.*;
+import java.util.Date;
+import android.graphics.Bitmap;;
 
+
+/**
+ * class used to store photos along with their author and date inserted
+ * @author jim
+ *
+ */
 public class Photo {
 
 	private String author;
-	//private BufferedImage photo;
+	private Bitmap photo;
+	private Date date;
 	
+	/**
+	 * photo class constructor
+	 * @param author
+	 * @param photo
+	 */
+	public Photo(String author, Bitmap photo) {
+		this.author = author;
+		this.photo = photo;
+		this.date = new Date();
+	}
+
+	/*
+	 * below are gets and setters of the class
+	 */
+	
+	public Bitmap getPhoto() {
+		return this.photo;
+	}
+	
+	public Date getDate() {
+		return this.date;
+	}
+	
+	public String getAuthor() {
+		return this.author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public void setPhoto(Bitmap photo) {
+		this.photo =  photo;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
