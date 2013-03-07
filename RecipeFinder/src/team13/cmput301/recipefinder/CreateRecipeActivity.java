38e35e582c192fde152353773d8cde5b9145aa66
hwen3@ualberta.ca <hwen3@ualberta.ca>
2013-03-06 21:41:15 -0700

    added base adapter class to work with gallery view

--- RecipeFinder/src/team13/cmput301/recipefinder/CreateRecipeActivity.java
+++ RecipeFinder/src/team13/cmput301/recipefinder/CreateRecipeActivity.java
@@ -3,13 +3,22 @@ package team13.cmput301.recipefinder;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
 public class CreateRecipeActivity extends Activity {
 
+	Gallery gallery;
+	ImageView imageView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_recipe);
+		
+		gallery = (Gallery)findViewById(R.id.gallery1);
+		//imageView = (ImageView)findViewById(R.id.i)
+		gallery.setAdapter(new ImageAdapter(this));
 	}
 
 	@Override
