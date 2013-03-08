5ccd29007e3555b45cbfe701644f7212c3733b2d
jessicayuen <jyuen@ualberta.ca>
2013-03-08 11:15:23 -0700

    Implemented add photo functionality for DisplayRecipeActivity

--- RecipeFinder/src/team13/cmput301/recipefinder/DisplayRecipeActivity.java
+++ RecipeFinder/src/team13/cmput301/recipefinder/DisplayRecipeActivity.java
@@ -1,11 +1,18 @@
 package team13.cmput301.recipefinder;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class DisplayRecipeActivity extends Activity {
 
+	final int FILE_PATH_REQUEST = 1; // request code
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,5 +25,31 @@ public class DisplayRecipeActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_display_recipe, menu);
 		return true;
 	}
+	
+	/**
+	 * Allows the user to add a photo on 'Add Photo' button click
+	 * @param view
+	 */
+	public void addPhoto(View view) {
+		/* show a message if fields not entered */
+		AlertDialog alertDialog = 
+				new AlertDialog.Builder(DisplayRecipeActivity.this).create();
+		alertDialog.setTitle("Add a Picture");
+		alertDialog.setButton(Dialog.BUTTON_POSITIVE, "Use Existing", 
+				new DialogInterface.OnClickListener() {
+			
+			public void onClick(DialogInterface dialog, int which) {
+				Intent intent = new Intent(DisplayRecipeActivity.this, 
+						FileExplorerActivity.class);
+				startActivityForResult(intent, FILE_PATH_REQUEST);
+			} 
+		});
+		alertDialog.setButton(Dialog.BUTTON_NEGATIVE, "Take a Picture", 
+				new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {} });
+		alertDialog.show();
+	}
+	
+	
 
 }
