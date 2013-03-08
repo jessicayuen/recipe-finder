842d0ffd1dd5934969ccb7610f6e00835a7b7f0b
jessicayuen <jyuen@ualberta.ca>
2013-03-08 12:23:49 -0700

    Implemented email sharing functionality for DisplayRecipe activity

--- RecipeFinder/src/team13/cmput301/recipefinder/DisplayRecipeActivity.java
+++ RecipeFinder/src/team13/cmput301/recipefinder/DisplayRecipeActivity.java
@@ -6,17 +6,25 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class DisplayRecipeActivity extends Activity {
 
-	final int FILE_PATH_REQUEST = 1; // request code
+	private final int FILE_PATH_REQUEST = 1; // request code
+	Recipe recipe;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_recipe);
+		
+		Bundle extras = getIntent().getExtras();
+		if (extras != null) {
+			recipe = (Recipe) extras.getParcelable("recipe");
+		}
 	}
 
 	@Override
@@ -50,6 +58,45 @@ public class DisplayRecipeActivity extends Activity {
 		alertDialog.show();
 	}
 	
-	
+	/**
+	 * Allows the user to email a recipe on 'Share' button click
+	 * @param view
+	 */
+	public void shareRecipe(View view) {
+		AlertDialog alertDialog =
+				new AlertDialog.Builder(DisplayRecipeActivity.this).create();
+		alertDialog.setTitle("Email Recipe");
+		alertDialog.setMessage("Enter recipient: ");
+		
+		// Set an EditText view to get user input
+		final EditText input = new EditText(this);
+		alertDialog.setView(input);
+		alertDialog.setButton(Dialog.BUTTON_POSITIVE, "Send", 
+				new DialogInterface.OnClickListener() {
 
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				String recipient = input.getText().toString();
+				Intent intent = new Intent(Intent.ACTION_SEND);
+				intent.setType("text/html");
+				intent.putExtra(Intent.EXTRA_EMAIL, recipient);
+				intent.putExtra(Intent.EXTRA_SUBJECT, 
+						"You have a new Recipe from " + 
+						User.getUser().getUsername() + "!");
+				intent.putExtra(Intent.EXTRA_TEXT, 
+						Html.fromHtml(recipe.toString()));
+				startActivity(intent);
+				return;
+			}
+		});
+		alertDialog.setButton(Dialog.BUTTON_NEGATIVE, "Cancel", 
+				new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				return;
+			}
+		});
+		alertDialog.show();
+	}
 }
