15e1af64a1f1cf1df7495bd12b4cecf5dba80390
jim <jim@jim-Lenovo-IdeaPad-Y580>
2013-03-10 00:05:54 -0700

    finished dialog to show list of items

--- RecipeFinder/src/team13/cmput301/recipefinder/CreateRecipeActivity.java
+++ RecipeFinder/src/team13/cmput301/recipefinder/CreateRecipeActivity.java
@@ -1,6 +1,7 @@
 package team13.cmput301.recipefinder;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -24,14 +25,18 @@ import android.widget.Toast;
 
 public class CreateRecipeActivity extends Activity {
 
-	final int FILE_PATH_REQUEST = 2; // request code
-	private boolean textChanged = false;
+	private static final int FILE_PATH_REQUEST = 2; // request code
+	private static final int INGREDIENTDIALOG = 1;
+	private static final int INSTRUCTIONDIALOG = 2;
+	private boolean textChanged = false, contentChanged = false;
 	private Button saveButton, exitButton, addPicButton, addIngredButton, addInsButton;
+	private Button ingredListButton, instrListButton;
 	private EditText addName, addIngredients, addInstructions, addDescription;
 	private Gallery gallery;
 	private ImageView imageView;
-	private ListView ingredListView, instrListView;
+	//private ListView ingredListView, instrListView;
 	private ArrayList<String> ingredients, instructions;
+	private List<Integer> mSelectedItems;
 	private ArrayAdapter<String> instrAdapter, ingredAdapter;
 
 	@Override
@@ -39,6 +44,8 @@ public class CreateRecipeActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_recipe);
 
+		ingredListButton = (Button) findViewById(R.id.ingredientListButton);
+		instrListButton = (Button) findViewById(R.id.instructionListButton);
 		saveButton = (Button) findViewById(R.id.createButton);
 		exitButton = (Button) findViewById(R.id.exitButton);
 		addPicButton = (Button) findViewById(R.id.addPicturesButn);
@@ -48,20 +55,20 @@ public class CreateRecipeActivity extends Activity {
 		addIngredients = (EditText) findViewById(R.id.addIngredients);
 		addInstructions = (EditText) findViewById(R.id.addInstructions);
 		addDescription = (EditText) findViewById(R.id.addDescription);
-		ingredListView = (ListView) findViewById(R.id.ingredientListView);
-		instrListView = (ListView) findViewById(R.id.instructionListView);
+		//		ingredListView = (ListView) findViewById(R.id.ingredientListView);
+		//		instrListView = (ListView) findViewById(R.id.instructionListView);
 
 		ingredients = new ArrayList<String>();
 		instructions = new ArrayList<String>();
-		
-		instrAdapter =      
-				new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, instructions);
-		instrListView.setAdapter(instrAdapter); 
-		
-		ingredAdapter =      
-				new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, ingredients);
-		ingredListView.setAdapter(ingredAdapter); 
-		
+
+		//		instrAdapter =      
+		//				new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, instructions);
+		//		instrListView.setAdapter(instrAdapter); 
+		//
+		//		ingredAdapter =      
+		//				new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, ingredients);
+		//		ingredListView.setAdapter(ingredAdapter); 
+
 		gallery = (Gallery) findViewById(R.id.gallery);
 		//imageView = (ImageView)findViewById(R.id.i)
 		gallery.setAdapter(new ImageAdapter(this));
@@ -81,8 +88,6 @@ public class CreateRecipeActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				addedName();
-				addedIngredients();
-				addedInstructions();
 				addedDescription();
 				if(textChanged) {
 
@@ -152,7 +157,9 @@ public class CreateRecipeActivity extends Activity {
 				if(tempStr.length() != 0) {
 					if(!ingredients.contains(tempStr)){
 						ingredients.add(tempStr);
-						ingredAdapter.notifyDataSetChanged();
+						addIngredients.setText("");
+						contentChanged = true;
+						//						ingredAdapter.notifyDataSetChanged();
 					} else {
 						toast = Toast.makeText(CreateRecipeActivity.this,
 								"Ingredient Already Exist", Toast.LENGTH_SHORT);
@@ -175,7 +182,9 @@ public class CreateRecipeActivity extends Activity {
 				// TODO Auto-generated method stub
 				if(tempStr.length() != 0){
 					instructions.add(tempStr);
-					instrAdapter.notifyDataSetChanged();
+					addInstructions.setText("");
+					contentChanged = true;
+					//					instrAdapter.notifyDataSetChanged();
 				} else {
 					toast = Toast.makeText(CreateRecipeActivity.this,
 							"No Text Entered", Toast.LENGTH_SHORT);
@@ -183,8 +192,35 @@ public class CreateRecipeActivity extends Activity {
 				}
 			}
 		});
+
+		ingredListButton.setOnClickListener(new View.OnClickListener() {
+
+			@SuppressWarnings("deprecation")
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				if(contentChanged){
+					removeDialog(INGREDIENTDIALOG);
+				}
+				showDialog(INGREDIENTDIALOG);
+			}
+		});
+
+		instrListButton.setOnClickListener(new View.OnClickListener() {
+
+			@SuppressWarnings("deprecation")
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				if(contentChanged){
+					removeDialog(INSTRUCTIONDIALOG);
+				}
+				showDialog(INSTRUCTIONDIALOG);
+			}
+		});
+
 	}
-	
+
 	/*
 	 * Obtain the filepath of the image returned from file explorer activity
 	 */
@@ -202,6 +238,103 @@ public class CreateRecipeActivity extends Activity {
 			}
 		}
 	}
+
+	protected Dialog onCreateDialog(int choice) {
+		Dialog dialog = null;
+		AlertDialog.Builder dialogBuilder;
+		mSelectedItems = new ArrayList<Integer>();  // Where we track the selected items
+		contentChanged = false;
+		
+		switch(choice){
+		case INGREDIENTDIALOG:
+			dialogBuilder = new AlertDialog.Builder(this);
+			CharSequence[] ingredCharSequence = ingredients.toArray(new CharSequence[ingredients.size()]);
+			// Set the dialog title
+			dialogBuilder.setTitle("Your Ingredients: ")
+			// Specify the list array, the items to be selected by default (null for none),
+			// and the listener through which to receive callbacks when items are selected
+			.setMultiChoiceItems(ingredCharSequence, null,
+					new DialogInterface.OnMultiChoiceClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which,
+						boolean isChecked) {
+					if (isChecked) {
+						// If the user checked the item, add it to the selected items
+						mSelectedItems.add(which);
+					} else if (mSelectedItems.contains(which)) {
+						// Else, if the item is already in the array, remove it 
+						mSelectedItems.remove(Integer.valueOf(which));
+					}
+				}
+			})
+			// Set the action buttons
+			.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int id) {
+					// User clicked OK, so save the mSelectedItems results somewhere
+					// or return them to the component that opened the dialog
+					for(int i = mSelectedItems.size() - 1; i >= 0; i--){
+						ingredients.remove(mSelectedItems.get(i).intValue());
+					}
+					contentChanged = true;
+				}
+			})
+			.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int id) {
+				}
+			});
+			dialog = dialogBuilder.create();
+			break;
+		case INSTRUCTIONDIALOG:
+			dialogBuilder = new AlertDialog.Builder(this);
+			for(int i = 0; i < instructions.size(); i++){
+				instructions.set(i, new String(i + 1 + ". " + instructions.get(i))); 
+			}
+			CharSequence[] instrCharSequence = instructions.toArray(new CharSequence[instructions.size()]);
+			// Set the dialog title
+			dialogBuilder.setTitle("Your Instructions: ")
+			// Specify the list array, the items to be selected by default (null for none),
+			// and the listener through which to receive callbacks when items are selected
+			.setMultiChoiceItems(instrCharSequence, null,
+					new DialogInterface.OnMultiChoiceClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which,
+						boolean isChecked) {
+					if (isChecked) {
+						// If the user checked the item, add it to the selected items
+						mSelectedItems.add(which);
+					} else if (mSelectedItems.contains(which)) {
+						// Else, if the item is already in the array, remove it 
+						mSelectedItems.remove(Integer.valueOf(which));
+					}
+				}
+			})
+			// Set the action buttons
+			.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int id) {
+					// User clicked OK, so save the mSelectedItems results somewhere
+					// or return them to the component that opened the dialog7
+					for(int i = mSelectedItems.size() - 1; i >= 0; i--){
+						instructions.remove(mSelectedItems.get(i).intValue());
+					}
+					contentChanged = true;
+				}
+			})
+			.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int id) {
+				}
+			});
+			dialog = dialogBuilder.create();
+			break;
+		default:
+			break;
+		}
+		return dialog;
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -250,45 +383,6 @@ public class CreateRecipeActivity extends Activity {
 					int after) {}
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {}
-		});
-	}
-	private void addedInstructions() {
-
-		addInstructions.addTextChangedListener(new TextWatcher(){
-			@Override
-			public void afterTextChanged(Editable s) {
-				if(s.toString().trim().length() != 0){
-					textChanged = true;
-				}
-				else{
-					textChanged = false;
-				}
-			}
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {}
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {}
-		});
-	}
-	private void addedIngredients() {
-
-		System.out.println("Came in");
-		addIngredients.addTextChangedListener(new TextWatcher(){
-			@Override
-			public void afterTextChanged(Editable s) {
-				if(s.toString().trim().length() != 0){
-					textChanged = true;
-				}
-				else{
-					textChanged = false;
-				}
-			}
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) { System.out.println("Came in2");}
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {System.out.println("Came in3");}
 		});
 	}
 }
