4a0697122fb473e97add0fd8d75dc6e1ac34e471
jessicayuen <jyuen@ualberta.ca>
2013-03-11 01:32:41 -0600

    Added test case for Recipe class

---
+++ RecipeFinderTest/src/RecipeTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import team13.cmput301.recipefinder.Photo;
+import team13.cmput301.recipefinder.Recipe;
+
+public class RecipeTest {
+
+	@Test
+	public void testRecipeConstructor() {
+		Recipe recipe = new Recipe("Name", "Description", "Author",
+				new ArrayList<String>(), new ArrayList<String>(), new ArrayList<Photo>());
+		
+		assertEquals("Name", recipe.getName());
+		assertEquals("Description", recipe.getDescription());
+		assertEquals("Author", recipe.getAuthor());
+		assertEquals(0, recipe.getIngredients().size());
+		assertEquals(0, recipe.getInstructions().size());
+		assertEquals(0, recipe.getPhotos().size());
+	}
+
+}
