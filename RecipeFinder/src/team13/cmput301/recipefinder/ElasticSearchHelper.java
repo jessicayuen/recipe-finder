164f6e277a3ca48762f0912f796a10ae1975da8a
shenwei <shenweiliao@gmail.com>
2013-03-11 00:05:18 -0600

    Try/catch some exceptions for keyword search.

--- RecipeFinder/src/team13/cmput301/recipefinder/ElasticSearchHelper.java
+++ RecipeFinder/src/team13/cmput301/recipefinder/ElasticSearchHelper.java
@@ -5,19 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -139,25 +136,34 @@ public class ElasticSearchHelper {
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				HttpGet searchRequest = new HttpGet(
-						"http://cmput301.softwareprocess.es:8080/cmput301w13t12/_search?pretty=1&q="
-								+ java.net.URLEncoder.encode(myString, "UTF-8"));
-				searchRequest.setHeader("Accept", "application/json");
-				HttpResponse response = httpclient.execute(searchRequest);
-				String status = response.getStatusLine().toString();
-				System.out.println(status);
+				HttpGet searchRequest;
+				try {
+					searchRequest = new HttpGet(
+							"http://cmput301.softwareprocess.es:8080/cmput301w13t12/_search?pretty=1&q="
+									+ java.net.URLEncoder.encode(myString,
+											"UTF-8"));
+					searchRequest.setHeader("Accept", "application/json");
+					HttpResponse response = httpclient.execute(searchRequest);
+					String status = response.getStatusLine().toString();
+					System.out.println(status);
+					String json = getEntityContent(response);
+					Type elasticSearchSearchResponseType = new TypeToken<ElasticSearchSearchResponse<Recipe>>() {
+					}.getType();
+					ElasticSearchSearchResponse<Recipe> esResponse = gson
+							.fromJson(json, elasticSearchSearchResponseType);
+					System.err.println(esResponse);
+					for (ElasticSearchResponse<Recipe> r : esResponse.getHits()) {
+						Recipe recipe = r.getSource();
+						System.err.println(recipe);
+					}
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
 
-				String json = getEntityContent(response);
-
-				Type elasticSearchSearchResponseType = new TypeToken<ElasticSearchSearchResponse<Recipe>>() {
-				}.getType();
-				ElasticSearchSearchResponse<Recipe> esResponse = gson.fromJson(
-						json, elasticSearchSearchResponseType);
-				System.err.println(esResponse);
-				for (ElasticSearchResponse<Recipe> r : esResponse.getHits()) {
-					Recipe recipe = r.getSource();
-					System.err.println(recipe);
 				}
+				
+				
 				// searchRequest.releaseConnection(); not available in android
 				// httpclient
 			}
