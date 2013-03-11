9c35c77d1077630c2cbed7dbfeb466df7940f8d3
shenwei <shenweiliao@gmail.com>
2013-03-11 10:37:40 -0600

    Change a misspelling of view

--- RecipeFinder/src/team13/cmput301/recipefinder/DisplayRecipeActivity.java
+++ RecipeFinder/src/team13/cmput301/recipefinder/DisplayRecipeActivity.java
@@ -157,7 +157,7 @@ public class DisplayRecipeActivity extends Activity {
     
 	/**
 	 * Allows the user to email a recipe on 'Share' button click
-	 * @param views
+	 * @param view
 	 */
 	public void shareRecipe(View view) {
 		AlertDialog alertDialog =
