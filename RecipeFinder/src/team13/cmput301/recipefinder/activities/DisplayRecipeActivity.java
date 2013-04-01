b0b818d2b2b0dadd4bef1740643ff1fe738cb7cd
jessicayuen <jyuen@ualberta.ca>
2013-04-01 16:56:26 -0600

    Refactoring using JDeodorant to DisplayRecipeActivity

--- RecipeFinder/src/team13/cmput301/recipefinder/activities/DisplayRecipeActivity.java
+++ RecipeFinder/src/team13/cmput301/recipefinder/activities/DisplayRecipeActivity.java
@@ -103,7 +103,6 @@ public class DisplayRecipeActivity extends Activity  {
 	 * Display the contents of the recipe.
 	 */
 	private void displayRecipe() {
-		TextView textView;
 		ImageView imageView;
 		String ingredients = "";
 		String instructions = "";
@@ -126,21 +125,8 @@ public class DisplayRecipeActivity extends Activity  {
 		}
 
 		recipeRating.setRating(recipe.getRating());
-
-		textView = (TextView) this.findViewById(R.id.authorInfo);
-		textView.setText(recipe.getAuthor());
-
-		textView = (TextView) this.findViewById(R.id.descriptionInfo);
-		textView.setText(recipe.getDescription());
-
-		textView = (TextView) this.findViewById(R.id.ingredientsInfo);
-		textView.setText(ingredients);
-
-		textView = (TextView) this.findViewById(R.id.instructionsInfo);
-		textView.setText(instructions);
 		
-		textView = (TextView) this.findViewById(R.id.dateInfo);
-		textView.setText(recipe.getDate().toString());
+		setTextViews(ingredients, instructions);
 	}
 
 	/**
@@ -211,12 +197,30 @@ public class DisplayRecipeActivity extends Activity  {
 	 * @param view The current activity view
 	 */
 	public void shareRecipe(View view) {
-		/* Set an EditText view to get user input */
-		final EditText input = new EditText(this);
+		final EditText input = setUpEmailText();
+		showEmailDialog(input);
+	}
+	
+	/**
+	 * Set up text box definitions for the email dialog box.
+	 * @return The EditText
+	 */
+	private EditText setUpEmailText() {
+		EditText input = new EditText(this);
+		
 		input.setHeight(200);
 		input.setHint("Seperate recipents by ,");
 		input.setGravity(0);
+		
+		return input;
+	}
 
+	/**
+	 * Show the email dialog box - prompts user to enter recipients
+	 * and sends on button click.
+	 * @param input The text box for recipients input
+	 */
+	private void showEmailDialog(final EditText input) {
 		/* Setup the alert dialog */
 		AlertDialog alertDialog =
 				new AlertDialog.Builder(DisplayRecipeActivity.this).create();
@@ -277,7 +281,7 @@ public class DisplayRecipeActivity extends Activity  {
 
 		alertDialog.show();
 	}
-
+	
 	/**
 	 * Add the recipe to the online database
 	 * @param view
@@ -369,5 +373,30 @@ public class DisplayRecipeActivity extends Activity  {
 				return false;
 			}			
 		});
+	}
+	
+
+	/**
+	 * Set up layout textViews based on recipe parameters.
+	 * @param ingredients The ingredients in the recipe
+	 * @param instructions The instructions in the recipe
+	 */
+	private void setTextViews(String ingredients, String instructions) {
+		TextView textView;
+		
+		textView = (TextView) this.findViewById(R.id.authorInfo);
+		textView.setText(recipe.getAuthor());
+
+		textView = (TextView) this.findViewById(R.id.descriptionInfo);
+		textView.setText(recipe.getDescription());
+
+		textView = (TextView) this.findViewById(R.id.ingredientsInfo);
+		textView.setText(ingredients);
+
+		textView = (TextView) this.findViewById(R.id.instructionsInfo);
+		textView.setText(instructions);
+		
+		textView = (TextView) this.findViewById(R.id.dateInfo);
+		textView.setText(recipe.getDate().toString());
 	}
 }
